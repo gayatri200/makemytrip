b2026e0bcc7240487e30cf02245f2c278fdbe387
gayatri20 <jyotivinodmali123@gmail.com>
2025-04-26 23:03:29 +0530

    makemytrip Code

---
+++ src/main/java/com/example/makemytrip/Bus.java
@@ -0,0 +1,15 @@
+package com.example.makemytrip;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class Bus {
+
+    @GetMapping("/Bus")
+    public String getData() {return  "Please book your bus tickets  from mmt kindly book ticket for New Delhi to anywhere at 10% discount" ;
+
+    }
+
+}
