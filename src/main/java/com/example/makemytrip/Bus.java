46fd57bf3a34d73723a8c0a46d9045e7b4469fa7
gayatri20 <jyotivinodmali123@gmail.com>
2025-04-27 09:48:15 +0530

    hello

--- src/main/java/com/example/makemytrip/Bus.java
+++ src/main/java/com/example/makemytrip/Bus.java
@@ -8,14 +8,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class Bus {
 
     @GetMapping("/Bus")
-<<<<<<< HEAD
-    public String getData() {return  "Please book your bus tickets  from mmt kindly book ticket for New Delhi " ;
-=======
+
 
     public String getData() {
-        System.out.println("develop A XYZ");
+        System.out.println("develop B XYZ");
         return  "Please book your bus tickets  from mmt kindly book ticket for New Delhi" ;
->>>>>>> 06a8086ebea8f9845b5a086dac53ef536a711947
 
     }
 
