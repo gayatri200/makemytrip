3da5dd49cc4cb55dd10245e096d284fc7e042d44
Gayatri mali <76807957+gayatri200@users.noreply.github.com>
2025-06-28 14:39:17 +0530

    Update Bus.java

--- src/main/java/com/example/makemytrip/Bus.java
+++ src/main/java/com/example/makemytrip/Bus.java
@@ -11,7 +11,7 @@ public class Bus {
 
     public String getData() {
 
-        System.out.println("develop Bus Service");
+        System.out.println("develop Bus Service  ");
 
         System.out.println("develop Gayatri");
         return  "Please book your bus tickets  from mmt kindly book ticket for easy travel" ;
